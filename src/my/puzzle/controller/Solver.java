08dff810b9741a20cd61e95c6b6732875723ca50
HJS <407410014@qq.com>
2017-11-02 16:40:33 +0800

    move Solver to folder controller and add logics

--- src/my/puzzle/controller/Solver.java
+++ src/my/puzzle/controller/Solver.java
@@ -1,17 +1,24 @@
+package my.puzzle.controller;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.Stack;
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdDraw;
+import my.puzzle.model.PuzzleBoard;
 
 public class Solver {
 
+    // delay in miliseconds (controls animation speed)
+    private static final int DELAY = 200;
+    
+    public static final int VK_SPACE = 0x20;
+    
     private int moves = 0;
 
     private boolean solvable = false;
 
-    private final Stack<Board> solution;
+    private final Stack<PuzzleBoard> solution;
 
-    public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
+    public Solver(PuzzleBoard initial)           // find a solution to the initial board (using the A* algorithm)
     {
         if (initial == null) throw new java.lang.IllegalArgumentException();
 
@@ -25,7 +32,7 @@ public class Solver {
         minPQ.insert(min);      // insert min
         twinPQ.insert(twin);      
         Priority sol = solve(minPQ, twinPQ);
-        
+
         solution = new Stack<>();
         while (sol != null) {       // get solution
             solution.push(sol.board);
@@ -37,13 +44,13 @@ public class Solver {
         Priority twin;
         while (true) {
             min = minPQ.delMin();   // find the smallest one
-            if (min.board.isGoal()) {   // solvable
+            if (min.board.getManhattan() == 0) {   // solvable
                 solvable = true;
                 moves = min.moves;
                 break;
             } 
             twin = twinPQ.delMin();   // find the smallest one
-            if (twin.board.isGoal()) {   // unsolvable
+            if (twin.board.getManhattan() == 0) {   // unsolvable
                 moves = -1;
                 break;
             } 
@@ -53,9 +60,9 @@ public class Solver {
         if (solvable)   return min;
         return null;
     }
-    private Iterable<Board> insertNeighbors(MinPQ<Priority> minPQ, Priority min) {
-        Iterable<Board> it = min.board.neighbors(); // get smallest one's neighbors
-        for (Board b : it) {
+    private Iterable<PuzzleBoard> insertNeighbors(MinPQ<Priority> minPQ, Priority min) {
+        Iterable<PuzzleBoard> it = min.board.neighbors(); // get smallest one's neighbors
+        for (PuzzleBoard b : it) {
             if (min.father == null || !b.equals(min.father.board))   minPQ.insert(new Priority(b, min));
         }
         return it;
@@ -69,22 +76,22 @@ public class Solver {
         if (!solvable)  return -1;
         return moves;
     }
-    public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
+    public Iterable<PuzzleBoard> solution()      // sequence of boards in a shortest solution; null if unsolvable
     {
         if (!solvable)  return null;
         return solution;
     }
     private class Priority implements Comparable<Priority> {
-        Board board;
+        PuzzleBoard board;
         int moves;
         int manhattan;
         Priority father;
-        
-        Priority(Board mBoard, Priority mFather) {
+
+        Priority(PuzzleBoard mBoard, Priority mFather) {
             this.board = mBoard;
             if (mFather == null)    this.moves = 0;
             else                    this.moves = mFather.moves + 1;
-            this.manhattan = board.manhattan(); 
+            this.manhattan = board.getManhattan(); 
             this.father = mFather;
         }
         int getPriority() {
@@ -97,25 +104,31 @@ public class Solver {
     }
     public static void main(String[] args) // solve a slider puzzle (given below)
     {
-        // create initial board from file
-        In in = new In(args[0]);
+        // turn on animation mode
+        StdDraw.enableDoubleBuffering();
+
+        In in = new In(args[0]);    // input file
         int n = in.readInt();
         int[][] blocks = new int[n][n];
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
+
+        for (int i = 0; i < blocks.length; i++) {
+            for (int j = 0; j < blocks[0].length; j++) {
                 blocks[i][j] = in.readInt();
-        Board initial = new Board(blocks);
+            }
+        }
+        PuzzleBoard board = new PuzzleBoard(blocks);
+        Game.draw(board, n);
+        StdDraw.show();
 
-        // solve the puzzle
-        Solver solver = new Solver(initial);
+//        while (true) {
+//            if (StdDraw.isKeyPressed(VK_SPACE))     break;
+//        }
+        Iterable<PuzzleBoard> ans = new Solver(board).solution();
 
-        // print solution to standard output
-        if (!solver.isSolvable())
-            StdOut.println("No solution possible");
-        else {
-            StdOut.println("Minimum number of moves = " + solver.moves());
-//            for (Board board : solver.solution())
-//                StdOut.println(board);
+        for (PuzzleBoard pb : ans) {
+            StdDraw.pause(DELAY);
+            Game.draw(pb, n);
+            StdDraw.show();
         }
     }
 }
